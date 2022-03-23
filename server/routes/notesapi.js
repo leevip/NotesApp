09304eca7f7fe2ffc52221254ae2e9ae24611895
leevi <leevi.pussinen@live.com>
2022-03-23 23:20:46 +0200

    fix

--- server/routes/notesapi.js
+++ server/routes/notesapi.js
@@ -1,31 +1,33 @@
 var express = require('express');
-const { body } = require('express-validator');
 var router = express.Router();
 const Note = require('../models/Note');
 const {v4: uuidv4} = require('uuid');
 
-
+//Get function, returns notes
 router.get('/notes', function(req, res, next) {
     console.log('Get /notes');
     Note.find({}, (err, notes) => {
         if (err) {
-            res.json({error: "Database error"});
+            res.json({error: "Database error"}); //If an error occurs the backend server sends error message to frontend
         } else {
             res.json(notes);
         }
     })
 });
+
+//Get function, returns topic filtered notes
 router.get('/notes/:topic', function(req, res, next) {
     console.log('Get /notes');
     Note.find({topic: req.params.topic}, (err, notes) => {
         if (err) {
-            res.json({error: "Database error"});
+            res.json({error: "Database error"}); //If an error occurs the backend server sends error message to frontend
         }
         res.json(notes);
     })
 });
 
-router.post('/note', body("content").notEmpty(), body("topic").notEmpty(), function(req, res, next) {
+//Post function, saves a new note to database and returns the note
+router.post('/note', function(req, res, next) {
     console.log('POST /note');
     Note.create({
         note_id: uuidv4(),
@@ -36,7 +38,7 @@ router.post('/note', body("content").notEmpty(), body("topic").notEmpty(), funct
     },
     (err, ok) => {
         if(err) {
-            res.json({error: "Error while creating note"});
+            res.json({error: "Error while creating note"}); //If an error occurs the backend server sends error message to frontend
         } else {
             return res.json(ok);
         }
